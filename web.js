4d606870163b5603d784c55f2101029ab4727b0c
Alpha Bah <alphaobah@zalph.com>
2013-07-20 00:46:17 +0000

    Updated web.js to read data from index.html file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var MAX = 1024;
+var buf = new Buffer(MAX);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2 ');
+  buf = fs.readFileSync('index.html');
+  console.log(buf.toString('utf8',0,buf.length);
+  response.send(buf.toString('utf8',0,buf.length));
+
+  //response.send('Hello World! 2 ');
 });
 
 var port = process.env.PORT || 5000;
